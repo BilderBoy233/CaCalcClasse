c606952aca7b6e565dff74d5e4e9b61b0105c320
BilderBoy233 <rafaelcouto110@gmail.com>
2026-02-27 14:36:42 -0300

    Contas.cs

--- SoCalcClasse/CaCalcClasse/Contas.cs
+++ SoCalcClasse/CaCalcClasse/Contas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace CaCalcClasse
 {
-    internal class Class1
+    internal class Contas
     {
+        //Propriedades
+        private double num1;
+
+        public double Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        private double num2;
+
+
+        public double Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+        //Construtores
+
+        /// <summary>
+        /// Construtor padrão, standard builder
+        /// </summary>
+        public Contas()
+        {
+            Num1 = 0;
+            Num2 = 0;
+        }
+
+        public Contas(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        //Métodos
     }
 }
