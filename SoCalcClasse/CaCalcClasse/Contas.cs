3bd74fc93c8036425ac01bdd42d6cfbfd97693d2
BilderBoy233 <rafaelcouto110@gmail.com>
2026-03-06 14:40:27 -0300

    Contas.cs

--- SoCalcClasse/CaCalcClasse/Contas.cs
+++ SoCalcClasse/CaCalcClasse/Contas.cs
@@ -29,14 +29,18 @@ namespace CaCalcClasse
         //Construtores
 
         /// <summary>
-        /// Construtor padrão, standard builder
+        /// Construtor padrão
         /// </summary>
         public Contas()
         {
             Num1 = 0;
             Num2 = 0;
         }
-
+        /// <summary>
+        /// Construtor com parâmetros
+        /// </summary>
+        /// <param name="num1">1º número</param>
+        /// <param name="num2">2º número</param>
         public Contas(double num1, double num2)
         {
             Num1 = num1;
@@ -44,5 +48,21 @@ namespace CaCalcClasse
         }
 
         //Métodos
+
+        public string Somar()
+        {
+            return "A some é " + (Num1 + Num2);
+        }
+        public string Subtrair()
+        {
+            return "A diferença é " + (Num1 + Num2);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" + 
+                $"Num1: {Num1}\n" +
+                $"Num2: " + Num2 + "\n";
+        }
     }
 }
