d787b660856b61e23f331c6b9192b1e9529dc5e8
BilderBoy233 <rafaelcouto110@gmail.com>
2026-04-14 08:12:17 -0300

    Program.cs

--- SoCalcClasse/CaCalcClasse/Program.cs
+++ SoCalcClasse/CaCalcClasse/Program.cs
@@ -1,18 +1,21 @@
 // See https://aka.ms/new-console-template for more information
 
 using CaCalcClasse;
+using System.Threading.Channels;
 
 Contas contas = new Contas(3, 44);
 
-Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Black;
 
-int opcao = "0";
+int opcao = 0;
+string resp = "";
 do
 {
+    //Menu
     Console.Clear();
     Console.WriteLine("Menu" +
-        "\n1. Somnar" +
+        "\n1. Somar" +
         "\n2. Subtrair" +
         "\n3. Multiplicar" +
         "\n4. Dividir" +
@@ -20,11 +23,14 @@ do
     Console.WriteLine("Escolha uma opção:");
     opcao = int.Parse(Console.ReadLine()!);
 
-    //Ler os valores
-    Console.WriteLine("Informe o primeiro numero: ");
-    contas.Num1 = double.Parse(Console.ReadLine()!);
-    Console.WriteLine("Informe o segundo numero: ");
-    contas.Num2 = double.Parse(Console.ReadLine()!);
+        if (opcao > 0 && opcao < 5)
+        {
+        //Ler os valores
+        Console.WriteLine("Informe o primeiro numero: ");
+        contas.Num1 = double.Parse(Console.ReadLine()!);
+        Console.WriteLine("Informe o segundo numero: ");
+        contas.Num2 = double.Parse(Console.ReadLine()!);
+        }
 
     //Fazer as ops
     switch (opcao)
@@ -32,23 +38,34 @@ do
         case 1://Somar
             Console.WriteLine(contas.Somar());
             break;
-        case 2:
+        case 2://Subtrair
+            Console.WriteLine(contas.Subtrair());
             break;
-        case 3:
+        case 3://Multiplicar
+            Console.WriteLine(contas.Multiplicacao());
             break;
-        case 4:
+        case 4://Dividir
+            Console.WriteLine(contas.Divisao());
             break;
         case 0:
-            Console.WriteLine("Adeus!");
+            Console.WriteLine("Adeus!");    
             break;
         default:
-
             break;
     }
 
-    //Console.WriteLine("Deseja continuar? (s/n)");
-    //resp = Console.ReadLine()!.ToLower();
+    if (opcao > 0 && opcao < 5)
+    {
+        Console.WriteLine("Deseja continuar? (s/n)");
+        resp = Console.ReadLine()!.ToLower();
+        if (resp == "n")
+        {
+            Console.WriteLine("Adeus!");
+            break;
+        }
+    }
+    
+}
 
-
-    while (resp == "s" || resp == "sim") ;
-
+while (opcao != 0 );
+    
