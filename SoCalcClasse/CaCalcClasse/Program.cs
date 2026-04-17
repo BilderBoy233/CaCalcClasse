0d050ca38987460e46c38cf28e2c5ead74251aea
BilderBoy233 <rafaelcouto110@gmail.com>
2026-04-17 09:48:53 -0300

    Program.cs

--- SoCalcClasse/CaCalcClasse/Program.cs
+++ SoCalcClasse/CaCalcClasse/Program.cs
@@ -1,19 +1,17 @@
-// See https://aka.ms/new-console-template for more information
-
 using CaCalcClasse;
-using System.Threading.Channels;
 
 Contas contas = new Contas(3, 44);
 
-Console.BackgroundColor = ConsoleColor.Red;
-Console.ForegroundColor = ConsoleColor.Black;
+// Cores
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Clear();
 
 int opcao = 0;
-string resp = "";
+
 do
 {
-    //Menu
-    Console.Clear();
+    // Menu
     Console.WriteLine("Menu" +
         "\n1. Somar" +
         "\n2. Subtrair" +
@@ -21,51 +19,48 @@ do
         "\n4. Dividir" +
         "\n0. Sair");
     Console.WriteLine("Escolha uma opção:");
+
     opcao = int.Parse(Console.ReadLine()!);
-
-        if (opcao > 0 && opcao < 5)
-        {
-        //Ler os valores
-        Console.WriteLine("Informe o primeiro numero: ");
-        contas.Num1 = double.Parse(Console.ReadLine()!);
-        Console.WriteLine("Informe o segundo numero: ");
-        contas.Num2 = double.Parse(Console.ReadLine()!);
-        }
-
-    //Fazer as ops
-    switch (opcao)
-    {
-        case 1://Somar
-            Console.WriteLine(contas.Somar());
-            break;
-        case 2://Subtrair
-            Console.WriteLine(contas.Subtrair());
-            break;
-        case 3://Multiplicar
-            Console.WriteLine(contas.Multiplicacao());
-            break;
-        case 4://Dividir
-            Console.WriteLine(contas.Divisao());
-            break;
-        case 0:
-            Console.WriteLine("Adeus!");    
-            break;
-        default:
-            break;
-    }
+    Console.Clear();
 
     if (opcao > 0 && opcao < 5)
     {
-        Console.WriteLine("Deseja continuar? (s/n)");
-        resp = Console.ReadLine()!.ToLower();
-        if (resp == "n")
-        {
-            Console.WriteLine("Adeus!");
-            break;
-        }
-    }
-    
-}
+        string resp;
 
-while (opcao != 0 );
-    
+        do
+        {
+            // Ler valores
+            Console.WriteLine("Informe o primeiro numero: ");
+            contas.Num1 = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Informe o segundo numero: ");
+            contas.Num2 = double.Parse(Console.ReadLine()!);
+
+            // Operação
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine($"Resultado: {contas.Somar()}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Resultado: {contas.Subtrair()}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Resultado: {contas.Multiplicar()}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Resultado: {contas.Dividir()}");
+                    break;
+            }
+
+            Console.WriteLine("Deseja continuar nesta operação? (s/n)");
+            resp = Console.ReadLine()!.ToLower();
+
+            Console.Clear();
+
+        } while (resp == "s");
+    }
+
+} while (opcao != 0);
+
+Console.WriteLine("Adeus!");
